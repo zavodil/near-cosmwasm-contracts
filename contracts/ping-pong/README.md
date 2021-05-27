4382353dc11e970e51bc17cfee5d92987bafd77a
orkunkl <kulceorkun@gmail.com>
2021-05-27 17:32:28 +0300

    Init ping-pong contract

---
+++ contracts/ping-pong/README.md
@@ -0,0 +1,40 @@
+# Ping Pong
+
+Purpose of this repo is showing basic functionalities of CosmWasm smart contracts: State, Messages, Handler
+Front facing dApp will be available soon.
+
+## Logic
+
+1. developer deploys smart contract and instantiates
+2  Frontend: user creates, signs and broadcasts `ExecuteMsg::Ping`
+3. Smart contract: keeps track of amount of ping requests in storage. Increments count and responds `pong` string
+in `Data` field and in `event`.
+4. Frontend: User queries total count of ping.
+
+## Messages
+
+```rust
+// Instantiates a smart contract instance with 0 pong set
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    // Pings contract, increases ping count
+    Ping {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetCount returns the current ping count as a json-encoded number
+    GetCount {},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CountResponse {
+    pub ping_count: i32,
+}
+```
---
+++ contracts/ping-pong/src/contract.rs
@@ -0,0 +1,58 @@
+use cosmwasm_std::{
+    entry_point, to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+};
+
+use crate::error::ContractError;
+
+// Note, you can use StdResult in some functions where you do not
+// make use of the custom errors
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    unimplemented!()
+}
+
+// And declare a custom Error variant for the ones where you will want to make use of it
+#[entry_point]
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    unimplemented!()
+}
+
+
+#[entry_point]
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cosmwasm_std::{coins, from_binary};
+
+    #[test]
+    fn proper_initialization() {
+        let mut deps = mock_dependencies(&[]);
+
+        let msg = InstantiateMsg { count: 17 };
+        let info = mock_info("creator", &coins(1000, "earth"));
+
+        // we can just call .unwrap() to assert this was a success
+        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(0, res.messages.len());
+
+        // it worked, let's query the state
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+        let value: CountResponse = from_binary(&res).unwrap();
+        assert_eq!(17, value.count);
+    }
+}
