126d73e4d7af1460e0493e5ce59e521cd8f51f99
orkunkl <kulceorkun@gmail.com>
2021-06-15 15:00:55 +0300

    Add messages

--- contracts/ping-pong/src/msg.rs
+++ contracts/ping-pong/src/msg.rs
@@ -1,21 +1,26 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+// Instantiates a smart contract instance with 0 pong set
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InstantiateMsg {
-}
+pub struct InstantiateMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
+    // Pings contract, increases ping count
+    Ping {},
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
+    // GetCount returns the current ping count as a json-encoded number
+    GetCount {},
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct CountResponse {
+    pub ping_count: u32,
 }
